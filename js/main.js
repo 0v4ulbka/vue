c0062ba69521a8df1144e74ed0d037c1a2f7ad2c
gr402_hudal <dimahudobin05@gmail.com>
2023-01-30 09:25:28 +0700

    ПР1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//создание экземпляра vue
+let app = new Vue({
+    el: '#app',     //подключение экземпляра к html странице
+    data: {     //место для хранения данных
+        product: "Socks",
+        description: 'A pair of warm, fuzzy socks'
+    }
+})
